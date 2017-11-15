4259d8b19ae2f239a42aff80834b0b149ef16111
sfinch6 <sfinch6@gatech.edu>
2017-11-15 00:48:16 -0500

    got them communicating and the basis of starting the games running. Marked some TODOs

--- client.java
+++ client.java
@@ -1,43 +1,70 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
  
-public class Client {
+public class client {
   
   public static void main(String[] args) throws IOException {
     
-    // Passing null to getByName() produces the
-    // special "Local Loopback" IP address, for
-    // testing on one machine w/o a network:
-    // Alternatively, you can use 
-    // the address or name:
-    // InetAddress addr = InetAddress.getByName("127.0.0.1");
-    // InetAddress addr = InetAddress.getByName("localhost");
+    // TODO: fix addr
     InetAddress addr = InetAddress.getByName(null);
-
-    System.out.println("addr = " + addr);
-    Socket socket = new Socket(addr, Server.PORT);
+    Socket socket = new Socket(addr, server.PORT);
+    Scanner scanner = new Scanner(System.in);
     
-    // Guard everything in a try-finally to make
-    // sure that the socket is closed:
-    try {
+    game: try {
       
-      System.out.println("socket = " + socket);
       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      
-      // Output is automatically flushed
-      // by PrintWriter:
       PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
       
-      for(int i = 0; i &lt; 10; i ++) {
-        out.println("howdy " + i);
-        String str = in.readLine();
-        System.out.println(str);
+      //check if user is ready
+      System.out.println("Ready to start game? (y/n): ");
+      String userInput = scanner.nextLine();
+      while (!userInput.equals("y") && !userInput.equals("n")) {
+        System.out.println("Must respond with y or n.");
+        userInput = scanner.nextLine();
       }
-      out.println("END");
+      if (userInput.equals("n")) break game;
+
+      //signals start of game to server
+      out.println();
+
+      while(true) {
+        String status = in.readLine();
+        
+        //TODO: figure out how to format the data... for now assuming a string concatenation deal
+        if (Integer.valueOf(status.charAt(0)) == 0) {
+          //play game
+          board(status);
+          userInput = scanner.next();
+          while (!validGuess(userInput)) {
+            System.out.println("Error! Please guess one letter.");
+            userInput = scanner.next();
+          }
+          out.println(userInput.toLowerCase());
+        
+        } else {
+          //print out message (minus message flag)
+          String message = status.substring(1);
+          System.out.println(message);
+          if (message.equals("Game Over!")) break game;
+        }
+      
+      }
     
     } finally {
-      System.out.println("closing...");
       socket.close();
     }
+  }
+
+  //TODO: implement
+  //prints out hangman board for user given status
+  private static void board(String status) {
+    System.out.println("BOARD");
+  }
+
+  //TODO: implement
+  //checks if user's guess is a single letter 
+  private static boolean validGuess(String userInput) {
+    return true;
   }
 }
--- server.java
+++ server.java
@@ -1,24 +1,33 @@
 import java.io.*;
 import java.net.*;
  
-public class Server {  
+public class server {  
   
   static final int PORT = 8080;
-  
+  static int sessions;
   public static void main(String[] args) throws IOException {
     ServerSocket s = new ServerSocket(PORT);
     System.out.println("Server Started");
-    
-    try {
+    sessions = 0;
+   
+   try {
       while(true) {
         // Blocks until a connection occurs:
         Socket socket = s.accept();
+        sessions++;
         try {
-          new PlayerThread(socket);
+          if (sessions <= 3) new PlayerThread(socket);
+          else {
+            
+            //TODO: server overload message
+            socket.close();
+            sessions--;
+          }
         } catch(IOException e) {
           // If it fails, close the socket,
           // otherwise the thread will close it:
           socket.close();
+          sessions--;
         }
       }
     
@@ -36,25 +45,15 @@ class PlayerThread extends Thread {
   public PlayerThread(Socket s) throws IOException {
     socket = s;
     in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    
-    // Enable auto-flush:
     out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-    
-    // If any of the above calls throw an 
-    // exception, the caller is responsible for
-    // closing the socket. Otherwise the thread
-    // will close it.
-    run(); // Calls run()
-  }
-  public void run() {
+
     try {
-      while (true) {  
-        String str = in.readLine();
-        if (str.equals("END")) break;
-        System.out.println("Echoing: " + str);
-        out.println(str);
-      }
-      System.out.println("closing...");
+      System.out.println("made it!");
+      out.println("you're playing :)");
+
+      String str = in.readLine();
+      System.out.println("now we're talking");
+      out.println("9Game Over!");
     
     } catch (IOException e) {
     
