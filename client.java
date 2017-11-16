db832ba513f81543250b3b39cf5e9455be205b53
sfinch6 <sfinch6@gatech.edu>
2017-11-15 21:25:04 -0500

    implemented client side

--- client.java
+++ client.java
@@ -3,68 +3,77 @@ import java.io.*;
 import java.util.*;
  
 public class client {
-  
-  public static void main(String[] args) throws IOException {
-    
-    // TODO: fix addr
-    InetAddress addr = InetAddress.getByName(null);
-    Socket socket = new Socket(addr, server.PORT);
-    Scanner scanner = new Scanner(System.in);
-    
-    game: try {
-      
-      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-      
-      //check if user is ready
-      System.out.println("Ready to start game? (y/n): ");
-      String userInput = scanner.nextLine();
-      while (!userInput.equals("y") && !userInput.equals("n")) {
-        System.out.println("Must respond with y or n.");
-        userInput = scanner.nextLine();
-      }
-      if (userInput.equals("n")) break game;
 
-      //signals start of game to server
-      out.println();
+    public static void main(String[] args) throws IOException {
+    
+        // TODO: fix addr
+        InetAddress addr = InetAddress.getByName(null);
+        Socket socket = new Socket(addr, server.PORT);
+        Scanner scanner = new Scanner(System.in);
+        
+        game: try {
 
-      while(true) {
-        String status = in.readLine();
-        System.out.println("status: " + status);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+          
+            //check if user is ready
+            System.out.print("\nReady to start game? (y/n): ");
+            String userInput = scanner.nextLine();
+            while (!userInput.equals("y") && !userInput.equals("n")) {
+                System.out.println("Must respond with y or n.");
+                userInput = scanner.nextLine();
+            }
+            if (userInput.equals("n")) break game;
+
+            //signals start of 
+            //game to server
+            System.out.println();
+            out.println();
+
+            while(true) {
+                String status = in.readLine();
+                if ((int) status.charAt(0) == 0) {
+                    //play game
+                    board(status);
+                    userInput = scanner.next().toLowerCase();
+                    while (!validGuess(userInput)) {
+                        System.out.print("Error! Please guess one letter: ");
+                        userInput = scanner.next();
+                    }
+                    out.println(userInput);
+                
+                } else {
+                    //print out message (minus message flag)
+                    String message = status.substring(1);
+                    System.out.println(message);
+                    if (message.equals("Game Over!")) break game;
+                }
+            }
         
-        //TODO: figure out how to format the data... for now assuming a string concatenation deal
-        if (status.charAt(0) == '0') {
-          //play game
-          board(status);
-          userInput = scanner.next().toLowerCase();
-          while (!validGuess(userInput)) {
-            System.out.println("Error! Please guess one letter.");
-            userInput = scanner.next();
-          }
-          out.println(userInput);
-        
-        } else {
-          //print out message (minus message flag)
-          String message = status.substring(1);
-          System.out.println(message);
-          if (message.equals("Game Over!")) break game;
+        } finally {
+            socket.close();
         }
       }
-    
-    } finally {
-      socket.close();
+
+      //prints out hangman board for user given status
+    private static void board(String status) {
+
+        int length = (int) status.charAt(1);
+        //updated word
+        for (int i = 2; i < length + 2; i++) {
+            System.out.print(status.charAt(i) + " ");
+        }
+        //updated incorrect guesses
+        System.out.print("\nIncorrect Guesses: ");
+        for (int i = length + 2; i < status.length(); i++) {
+            System.out.print(status.charAt(i) + " ");
+        }
+        //new guess
+        System.out.print("\nLetter to guess: ");
     }
-  }
 
-  //TODO: implement
-  //prints out hangman board for user given status
-  private static void board(String status) {
-    System.out.println("BOARD");
-  }
-
-  //checks if user's guess is a single letter 
-  private static boolean validGuess(String userInput) {
-    return userInput.length() == 1 && userInput.charAt(0) <= 'z' && userInput.charAt(0)
-     >= 'a';
-  }
+    //checks if user's guess is a single letter 
+    private static boolean validGuess(String userInput) {
+        return userInput.length() == 1 && userInput.charAt(0) <= 'z' && userInput.charAt(0) >= 'a';
+    }
 }
--- server.java
+++ server.java
@@ -3,66 +3,62 @@ import java.net.*;
  
 public class server {  
   
-  static final int PORT = 8080;
-  static int sessions;
-  public static void main(String[] args) throws IOException {
-    ServerSocket s = new ServerSocket(PORT);
-    System.out.println("Server Started");
-    sessions = 0;
-   
-   threads: try {
-      while(true) {
-        // Blocks until a connection occurs:
-        Socket socket = s.accept();
-        sessions++;
-        try {
-          
-          if (sessions <= 3) new PlayerThread(socket);
-          else {
-            //server overload message
-            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-            out.println("17server-overloaded");
-            break threads;
-
-          }
-        } catch(IOException e) {
-          // If it fails, close the socket,
-          // otherwise the thread will close it:
-          break threads;
+    static final int PORT = 8080;
+    static int sessions;
+    public static void main(String[] args) throws IOException {
+        ServerSocket s = new ServerSocket(PORT);
+        System.out.println("Server Started");
+        sessions = 0;
+       
+       threads: try {
+            while(true) {
+                // Blocks until a connection occurs:
+                Socket socket = s.accept();
+                sessions++;
+                try {
+                  
+                    if (sessions <= 3) new PlayerThread(socket);
+                    else {
+                        //server overload message
+                        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                        out.println("17server-overloaded");
+                        break threads;
+                    }
+                } catch(IOException e) {
+                    // If it fails, close the socket,
+                    // otherwise the thread will close it:
+                    break threads;
+                }
+            }
+        
+        } finally {
+            s.close();
+            sessions--;
         }
-      }
-    
-    } finally {
-      s.close();
-      sessions--;
-    }
-  } 
-}
+      } 
+  }
 
 class PlayerThread extends Thread {
-  private Socket socket;
-  private BufferedReader in;
-  private PrintWriter out;
-  
-  public PlayerThread(Socket s) throws IOException {
-    socket = s;
-    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-
-    try {
-      System.out.println("made it!");
-      out.println("9you're playing :)");
-
-      String str = in.readLine();
-      System.out.println("now we're talking");
-      out.println("0word");
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
     
-    } catch (IOException e) {
-    
-    } finally {
-      try {
-        socket.close();
-      } catch(IOException e) {}
+    public PlayerThread(Socket s) throws IOException {
+        socket = s;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+        try {
+            String str = in.readLine();
+            //word guessed a, b and o
+            out.println("" + (char) 0 + (char) 4 + "_o__ab");
+        
+        } catch (IOException e) {
+        
+        } finally {
+            try {
+                socket.close();
+            } catch(IOException e) {}
+        }
     }
-  }
 }
