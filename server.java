5e1f4125e6896d9cce8a524e69198d0bf5af8bd2
michaelchen523 <michaelchen523@gmail.com>
2017-11-19 18:04:46 -0500

    Input arguments fully functional

--- server.java
+++ server.java
@@ -1,21 +1,48 @@
 import java.io.*;
 import java.net.*;
+import java.lang.Exception;
 
 public class server {
     //TODO read port from command line
-    static final int PORT = 8080;
+    static int PORT;
     static public int sessions;
     static protected String[] words;
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         Socket socket = null;
         System.out.println("Server Started");
-        System.out.println(args.length);
-        words = new String[]{"foo", "bar", "baz", "sonika", "michael", "hummus", "hodor", "sun",
-                 "flower", "capital", "amazon", "seattle", "burdell", "computer",
-                  "monet"};
-
-
+        if(args.length < 1 || args.length > 2) {
+            System.out.println("Incorrect number of arguments.");
+            System.exit(0);
+        } else {
+                String portAsString = args[0];
+            try {
+                PORT = Integer.parseInt(args[0]);
+            } catch(NumberFormatException e) {
+                System.out.println("First argument not a number");
+                System.exit(0);
+            }
+            if(args.length == 2) {
+                String filename = args[1];
+                File file = new File(filename);
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String header = br.readLine();
+                String[] headerInfo = header.split(" ");
+                int wordSize = Integer.parseInt(headerInfo[0]);
+                int arraySize = Integer.parseInt(headerInfo[1]);
+                words = new String[arraySize];
+                String str;
+                int idx = 0;
+                while((str = br.readLine()) != null) {
+                    words[idx] = str;
+                }
+            } else {
+                words = new String[]{"foo", "bar", "baz", "sonika", "michael",
+                    "hummus", "hodor", "sun", "flower", "capital", "amazon",
+                    "seattle", "burdell", "computer", "monet"};
+            }
+        }
+        
         try {
              serverSocket = new ServerSocket(PORT);
         } catch (IOException e) {
